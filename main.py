c7e2cac711bc512610d07939945eed441b085e44
Hidy97 <csaladi.alexandra@gmail.com>
2023-02-01 15:21:02 +0100

    Paraméterek elhelyezése és rövid elágazás elkészítése

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # megoldás
 
-def eredmeny():
-    pass
+def eredmeny(jatekos_pontjai, gep_pontjai):
+    if jatekos_pontjai > 21:
+        print("Játékos vesztett")
+    elif gep_pontjai > 21:
+        print("Gép vesztett")
+    else:
+        print("Mindkét fél lapja kevesebb mint 21")
 
 # tesztesetek
