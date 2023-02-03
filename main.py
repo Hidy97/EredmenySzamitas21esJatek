76956e7e6b71f5f32c3c0cb00aff9b2d562aa732
Hidy97 <csaladi.alexandra@gmail.com>
2023-02-03 13:31:02 +0100

    További tesztesetek megírása

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def eredmeny(jatekos_lapok: [int], gep_lapok: [int]):
 # tesztesetek
 
 
-def jatekos_vesztett_teszt():
+def jatekos_vesztett_teszt_nagyobb_pontszam():
     jatekos_lista = [9, 10, 2, 8]
     gep_lista = [5, 4, 3, 1]
     kapott_eredmeny = eredmeny(jatekos_lista, gep_lista)
@@ -31,11 +31,14 @@ def jatekos_vesztett_teszt():
         print("A teszt sikeres!")
     else:
         print("A teszt sikertelen!")
+    return kapott_eredmeny
 
 
-def jatekos_vesztett_teszt2():
+def jatekos_vesztett_teszt_nagyobb_lapokszama():
     jatekos_lista = [9, 10, 2, 8]
     gep_lista = [5, 4, 3]
+    kapott_eredmeny = eredmeny(jatekos_lista, gep_lista)
+    vart_eredmeny = "Játékos vesztett"
 
     if len(jatekos_lista) > len(gep_lista):
         print("Játékos vesztett! Neki van több lapja van.")
@@ -45,9 +48,34 @@ def jatekos_vesztett_teszt2():
         print("A teszt sikertelen állapotban van!")
 
 
+def jatekos_vesztett_teszt_gepnel_es_21nel_kisebb():
+    jatekos_lista = [9, 10, 2, 8]
+    gep_lista = [5, 4, 3]
+    kapott_eredmeny = eredmeny(jatekos_lista, gep_lista)
+    vart_eredmeny = "Mindkét fél lapja kevesebb mint 21"
+
+    if kapott_eredmeny == vart_eredmeny:
+        print("Játékos vesztett!")
+    else:
+        print("A teszt sikertelen!")
+
+
+def jatekos_vesztett_teszt_minden_egyenlo():
+    jatekos_lista = [9, 10, 2, 8]
+    gep_lista = [5, 4, 3]
+    kapott_eredmeny = eredmeny(jatekos_lista, gep_lista)
+    vart_eredmeny = "Mindkét fél lapja kevesebb mint 21"
+    kapott_eredmeny2 = jatekos_vesztett_teszt_nagyobb_pontszam()
+    if kapott_eredmeny == vart_eredmeny and kapott_eredmeny2:
+        print("Játékos vesztett!")
+    else:
+        print("A teszt sikertelen!")
+
+
 def tesztek():
-    jatekos_vesztett_teszt()
-    jatekos_vesztett_teszt2()
+    jatekos_vesztett_teszt_nagyobb_pontszam()
+    jatekos_vesztett_teszt_nagyobb_lapokszama()
+    jatekos_vesztett_teszt_gepnel_es_21nel_kisebb()
 
 
 tesztek()
