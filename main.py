a7747f5017f3ca6f825e8acd65f22ef19dc4c002
Hidy97 <csaladi.alexandra@gmail.com>
2023-02-09 11:55:54 +0100

    Eredmény metódus átfaragása kész, tesztesetek további módosítást igényelnek.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def pontszamitas(lapok: [list]):
 # megoldás
 
 
+"""
 def eredmeny(jatekos_lapok: [int], gep_lapok: [int]):
     jatekos_pontok: int = pontszamitas(jatekos_lapok)
     gep_pontok: int = pontszamitas(gep_lapok)
@@ -16,13 +17,41 @@ def eredmeny(jatekos_lapok: [int], gep_lapok: [int]):
     else:
         szoveg = "Mindkét fél lapja kevesebb mint 21"
     return szoveg
+"""
+
+
+def eredmeny(jatekos_lapok: [int], gep_lapok: [int]):
+    jatekos_pontok: int = pontszamitas(jatekos_lapok)
+    gep_pontok: int = pontszamitas(gep_lapok)
+    szoveg = ""
+
+    if jatekos_pontok <= 21 and jatekos_pontok <= 21:
+        if jatekos_pontok > gep_pontok:
+            szoveg = "Játékos nyert"
+        elif gep_pontok > jatekos_pontok:
+            szoveg = "Gép nyert"
+        elif gep_pontok == jatekos_pontok:
+            if len(jatekos_lapok) < len(gep_lapok):
+                szoveg = "Játékos nyert"
+            elif len(jatekos_lapok) < len(gep_lapok):
+                szoveg = "Gép nyert"
+            else:
+                szoveg = "Döntetlen, osztoztok a nyereségen!"
+    else:
+        if jatekos_pontok > 21:
+            szoveg = "Játékos vesztett"
+        elif gep_pontok > 21:
+            szoveg = "Gép vesztett"
+        elif jatekos_pontok > 21 and gep_pontok > 21:
+            szoveg = "Döntetlen! A Ház nyert!"
+    return szoveg
 
 # tesztesetek
 
 # 1.teszteset
 
 
-def jatekos_vesztett_nagyobb_pontszam_teszt():
+def jatekos_vesztett_nagyobbmint_21_teszt():
     print("Eset: amikor a játékos pontszáma nagyobb mint 21.\n\t")
     jatekos_lista = [9, 10, 2, 8]
     gep_lista = [5, 4, 3, 1]
@@ -41,19 +70,10 @@ def jatekos_vesztett_nagyobb_pontszam_teszt():
 
 def jatekos_vesztett_nagyobb_lapokszama_teszt():
     print("Eset: amikor a játékos lapja több mint a gépé.\n\t")
-    jatekos_lista = [9, 10, 2, 8]
+    jatekos_lista = [7, 3, 1, 8]
     gep_lista = [5, 4, 3]
     kapott_eredmeny = eredmeny(jatekos_lista, gep_lista)
     vart_eredmeny = "Játékos vesztett"
-
-    """
-        if len(jatekos_lista) > len(gep_lista):
-            print("Játékos vesztett! Neki van több lapja van.")
-        elif len(jatekos_lista) == len(gep_lista):
-            print("Egyenlö lapjuk van!")
-        else:
-            print("A teszt sikertelen állapotban van!")
-    """
 
     if kapott_eredmeny == vart_eredmeny:
         print("Játékos vesztett!")
@@ -65,9 +85,9 @@ def jatekos_vesztett_nagyobb_lapokszama_teszt():
 
 
 def jatekos_vesztett_gepnel_es_21nel_kisebb_teszt():
-    print("Eset: amikor a játékos a gépnél is és 21nél is kisebb a pontszámmal rendelkezik.\n\t")
-    jatekos_lista = [9, 10, 2, 8]
-    gep_lista = [5, 4, 3, 1]
+    print("Eset: amikor a játékos veszít kevesebb lappal és kevesebb pontszámmal.\n\t")
+    jatekos_lista = [10, 9]
+    gep_lista = [5, 4, 3, 7, 1]
     kapott_eredmeny = eredmeny(jatekos_lista, gep_lista)
     vart_eredmeny = ""
 
@@ -81,10 +101,10 @@ def jatekos_vesztett_gepnel_es_21nel_kisebb_teszt():
 # 4.teszteset
 
 
-def gep_vesztett_nagyobb_pontszam_teszt():
+def gep_vesztett_nagyobbmint_21_teszt():
     print("Eset: amikor a gép pontszáma nagyobb mint 21.\n\t")
-    jatekos_lista = [9, 10, 2, 8]
-    gep_lista = [5, 4, 3, 1]
+    jatekos_lista = [1, 4, 2, 7]
+    gep_lista = [5, 9, 3, 6]
     kapott_eredmeny = eredmeny(jatekos_lista, gep_lista)
     vart_eredmeny = "Gép vesztett"
 
@@ -105,12 +125,11 @@ def gep_vesztett_nagyobb_lapokszama_teszt():
     kapott_eredmeny = eredmeny(jatekos_lista, gep_lista)
     vart_eredmeny = "Gép vesztett"
 
-    if len(jatekos_lista) < len(gep_lista):
+    if kapott_eredmeny == vart_eredmeny:
         print("Gép vesztett")
-    elif len(jatekos_lista) == len(gep_lista):
-        print("Egyenlö lapjuk van!")
     else:
         print("A teszt sikertelen!")
+    return kapott_eredmeny
 
 # 6.teszteset
 
@@ -149,11 +168,11 @@ def jatekos_vesztett_minden_egyenlo_teszt():
 
 def tesztek():
     # játékos ellenőrzése
-    jatekos_vesztett_nagyobb_pontszam_teszt()
+    jatekos_vesztett_nagyobbmint_21_teszt()
     jatekos_vesztett_nagyobb_lapokszama_teszt()
     jatekos_vesztett_gepnel_es_21nel_kisebb_teszt()
     # gép ellenőrzése
-    gep_vesztett_nagyobb_pontszam_teszt()
+    gep_vesztett_nagyobbmint_21_teszt()
     gep_vesztett_nagyobb_lapokszama_teszt()
     gep_vesztett_jatekosnal_es_21nel_kisebb_teszt()
     # egyéb
